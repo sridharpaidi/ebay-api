b93fc4bdcacccb23bb5df604654fac04439a2f33
KevinGay <12kgay@gmail.com>
2021-05-09 21:27:44 -0400

    more work on configureProfile script

--- src/scripts/configureProfile.js
+++ src/scripts/configureProfile.js
@@ -1,5 +1,24 @@
-const aws = require('aws-sdk');
+const AWS = require('aws-sdk');
 
-console.log(aws.config);
-console.log(aws.config.credentials.accessKeyId);
-console.log(aws.config.credentials.secretAccessKey);
+const RoleArn = `arn:aws:iam::${process.env.ACCOUNT_ID}:role/CodeStarWorker-ebay-api-ToolChain`;
+
+console.log(roleArn);
+
+const sts = new AWS.STS();
+
+sts.assumeRole({ RoleArn, RoleSessionName: 'ebay-api' }, function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    // successful response
+    AWS.config.update({
+      accessKeyId: data.Credentials.AccessKeyId,
+      secretAccessKey: data.Credentials.SecretAccessKey,
+      sessionToken: data.Credentials.SessionToken,
+    });
+
+    console.log(AWS.config);
+    console.log(AWS.config.credentials.accessKeyId);
+    console.log(AWS.config.credentials.secretAccessKey);
+  }
+});
